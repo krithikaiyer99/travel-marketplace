c2af5ec51287920c2170c80a976536234215f709
krithikaiyer99 <iyer.kr@northeastern.edu>
2023-05-18 18:02:06 -0700

    search box complete

--- src/Home/SearchBox.js
+++ src/Home/SearchBox.js
@@ -1,19 +1,51 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { faMapMarkerAlt, faCalendar } from "@fortawesome/free-solid-svg-icons";
 
 import "./index.css";
 
 const SearchBox = () => {
-  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItem, setSelectedItem] = useState("Stays");
   const [numTravelers, setNumTravelers] = useState(1);
+  const [showLocations, setShowLocations] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [checkInDate, setCheckInDate] = useState(null);
+  const [checkOutDate, setCheckOutDate] = useState(null);
+  const [suggestions, setSuggestions] = useState([
+    "New York, NY",
+    "Seattle, WA",
+    "Boston, MA",
+  ]);
 
   const handleItemClick = (item) => {
     setSelectedItem(item);
+    setSearchQuery(item);
   };
 
   const handleNumTravelersChange = (event) => {
     setNumTravelers(event.target.value);
+  };
+  const handleLocationClick = () => {
+    setShowLocations(!showLocations);
+  };
+
+  const handleSearchChange = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+
+    // Perform search logic here to fetch suggestions based on the query
+    const fetchedSuggestions = []; // Replace with your own search logic
+    setSuggestions(fetchedSuggestions);
+  };
+
+  const handleCheckInDateChange = (date) => {
+    setCheckInDate(date);
+  };
+
+  const handleCheckOutDateChange = (date) => {
+    setCheckOutDate(date);
   };
 
   return (
@@ -74,22 +106,48 @@ const SearchBox = () => {
             </select>
           </div>
           <div className="search-box-search-field">
-            <div className="search-box-where">
+            <div className="search-box-where" onClick={handleLocationClick}>
               <FontAwesomeIcon icon={faMapMarkerAlt} />{" "}
-              <span className="placeholder">Going to</span>
+              <input
+                type="text"
+                className="search-input"
+                placeholder="Going to"
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+              {showLocations && (
+                <ul className="suggestions">
+                  {suggestions.map((item, index) => (
+                    <li key={index} onClick={() => handleItemClick(item)}>
+                      <FontAwesomeIcon icon={faMapMarkerAlt} />
+                      {" " + item}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
             <div className="search-box-checkinout">
               <FontAwesomeIcon icon={faCalendar} />
-              <span className="placeholder">Check-in</span>
+              <DatePicker
+                selected={checkInDate}
+                onChange={handleCheckInDateChange}
+                placeholderText="Check-in"
+                className="datepicker-input"
+              />
             </div>
             <div className="search-box-checkinout">
               <FontAwesomeIcon icon={faCalendar} />
-              <span className="placeholder">Check-out</span>
+              <DatePicker
+                selected={checkOutDate}
+                onChange={handleCheckOutDateChange}
+                placeholderText="Check-out"
+                className="datepicker-input"
+              />
             </div>
           </div>
           <div className="button-container">
             <div className="search-go-button">
-              <span >Search</span>
+              <span>Search</span>
             </div>
           </div>
         </div>
